ad94b310a9014dbc0f4d44c19d7f16cdb9822d87
TharrisonCV <159302123+TharrisonCV@users.noreply.github.com>
2025-01-17 09:46:41 -0600

    triangle

---
+++ C# Projects/triangle/Program.cs
@@ -0,0 +1,22 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+for (int i = 6; i > 0; i--)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
+
+
+Console.WriteLine();
+
+for (int i = 1; i <= 6; i++)
+{
+    for (int j = 0; j < i; j++)
+    {
+        Console.Write("*");
+    }
+    Console.WriteLine();
+}
